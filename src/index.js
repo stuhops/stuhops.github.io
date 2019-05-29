6341bc8fe1b4b713e7f9c64139cad45572b4acff
Stuart Hopkins <stuart.hopkins@aggiemail.usu.edu>
2019-05-29 08:50:43 -0600

    Asserted the created function was working

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
 var welcome = new Vue({
     el: '#theVue',
     data: {
-        jumboStu: [{'class': 'hidden'}],
+        jumboStu: '',
     },
     methods: {
+        fadein: function(el) {
+            let element = document.getElementById(el);
+            let op_inv = 1;
+            let op = 0;
+            var timer = setInterval(function () {
+               if(op > .9) {
+                   clearInterval(timer);
+                   op = 1;
+               }
+               element.opacity = op;
+               element.style.filter = 'alpha(opacity=' + op * 100 + ')';
+               op_inv -= op_inv * 0.1;
+               op = 1 - op_inv;
+            }, 50);
 
+        }
     },
     created(){
-
+        // fadein(this.jumboStu);
+        this.jumboStu.setAttribute('opacity', '0');
+        this.jumboStu = 'Stuart Hopkins';
     },
 
 });
