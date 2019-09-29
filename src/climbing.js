5c66b61b61d9c15d716830c280a496a0fd338971
stuhops <stuart.hopkins@aggiemail.usu.edu>
2019-09-29 13:09:53 -0600

    All the climber information is now recorded

--- src/climbing.js
+++ src/climbing.js
@@ -11,6 +11,14 @@ var climbing = new Vue({
             .then(user => user.json())
             .then(user=> {
                 console.log(user);
+                this.climber['name'] = user.name;
+                this.climber['picture'] = user.avatar;
+                this.climber['began'] = user.memberSince;
+                this.climber['home'] = user.location;
+                this.climber['sport'] = user.styles.Sport.lead;
+                this.climber['trad'] = user.styles.Trad.lead;
+                this.climber['profile'] = user.url;
+                console.log(this.climber);
             });
         fetch('https://www.mountainproject.com/data/get-ticks?email=stwhop@gmail.com&key=200196715-97b9b2bc4fd25b11ebc4a5cf9d9fba13')
             .then(ticks => ticks.json())
