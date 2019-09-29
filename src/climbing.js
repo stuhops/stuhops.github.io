2bffca7701a79fb64fd2a7b4b7d35420a9ca1757
stuhops <stuart.hopkins@aggiemail.usu.edu>
2019-09-29 14:14:37 -0600

    The call is now getting the tick data besides the actual route information

--- src/climbing.js
+++ src/climbing.js
@@ -10,7 +10,6 @@ var climbing = new Vue({
         fetch('https://www.mountainproject.com/data/get-user?email=stwhop@gmail.com&key=200196715-97b9b2bc4fd25b11ebc4a5cf9d9fba13')
             .then(user => user.json())
             .then(user=> {
-                console.log(user);
                 this.climber['name'] = user.name;
                 this.climber['picture'] = user.avatar;
                 this.climber['began'] = user.memberSince;
@@ -18,13 +17,34 @@ var climbing = new Vue({
                 this.climber['sport'] = user.styles.Sport.lead;
                 this.climber['trad'] = user.styles.Trad.lead;
                 this.climber['profile'] = user.url;
-                console.log(this.climber);
             });
         fetch('https://www.mountainproject.com/data/get-ticks?email=stwhop@gmail.com&key=200196715-97b9b2bc4fd25b11ebc4a5cf9d9fba13')
             .then(ticks => ticks.json())
             .then(ticks => {
                 console.log(ticks);
-            });
+                for(let i = 0; this.tickList.length < 6; i++) {
+                    let tmp = {};
+                    tmp['data'] = ticks.ticks[i].date;
+                    tmp['notes'] = ticks.ticks[i].notes;
+                    tmp['pitches'] = ticks.ticks[i].pitches;
+                    tmp['id'] = ticks.ticks[i].routeId;
+                    tmp['leadStyle'] = ticks.ticks[i].leadStyle;
+
+                    if (tmp.leadStyle == 'Redpoint' || tmp.leadStyle == 'Flash' || tmp.leadStyle == 'Onsite') {
+                        this.tickList.push(tmp)
+                    }
+                }
+            })
+            // .then(fetch('https://www.mountainproject.com/data/get-routes?routeIds=105869796&key=200196715-97b9b2bc4fd25b11ebc4a5cf9d9fba13')
+            //     .then(route => route.json())
+            //     .then(route => {
+            //     console.log(route);
+            //     // this.tickLis['name'] = routes[0].name;
+            //     // tmp['type'] = routes[0].type;
+            //     // tmp['stars'] = routes[0].stars;
+            //     // tmp['location'] = routes[0].location;
+
+            }));
     }
 
 });
