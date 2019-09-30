1e3e6ce708f07ad4db52d73a527ee5f722507d33
stuhops <stuart.hopkins@aggiemail.usu.edu>
2019-09-29 21:37:25 -0600

    Got the location to display

--- src/climbing.js
+++ src/climbing.js
@@ -3,6 +3,7 @@ var app = new Vue({
     data:{
         climber: {name: 'Loading...'},
         tickList: [{'name': 'Loading....'}],
+        toDoList: [{'name': 'Loading....'}],
     },
     methods:{ },
 
@@ -47,14 +48,48 @@ var app = new Vue({
                             tmp['grade'] = route.routes[0].rating;
                             tmp['type'] = route.routes[0].type;
                             tmp['stars'] = route.routes[0].stars;
-                            tmp['location'] = route.routes[0].location;
+                            tmp['city'] = route.routes[0].location[2];
+                            tmp['state'] = route.routes[0].location[0];
                             bigTmp.splice(i, 1, tmp);
                         })
                 }
             })
             .then(ticks => {
                 this.tickList = bigTmp;
-                console.log(this.tickList);
+            });
+
+        // Call for the to-do list
+        let bigTmp2 = [{}];
+        fetch('https://www.mountainproject.com/data/get-to-dos?email=stwhop@gmail.com&key=200196715-97b9b2bc4fd25b11ebc4a5cf9d9fba13')
+            .then(toDos => toDos.json())
+            .then(toDos => {
+                for(let i = 0; i < 3; i++) {
+                    let tmp = {};
+                    tmp['id'] = toDos.toDos[i];
+                    bigTmp2.push(tmp);
+                }
+                bigTmp2.shift();
+            })
+            .then(toDos => {
+                for(let i = 0; i < bigTmp2.length; i++) {  // the 'empty array' actually has an initial element so to get 6 entries offset
+                    fetch('https://www.mountainproject.com/data/get-routes?routeIds=' + bigTmp2[i].id +
+                        '&key=200196715-97b9b2bc4fd25b11ebc4a5cf9d9fba13')
+                        .then(route => route.json())
+                        .then(route => {
+                            let tmp = bigTmp2[i];
+                            tmp['name'] = route.routes[0].name;
+                            tmp['img'] = route.routes[0].imgMedium;
+                            tmp['grade'] = route.routes[0].rating;
+                            tmp['type'] = route.routes[0].type;
+                            tmp['stars'] = route.routes[0].stars;
+                            tmp['city'] = route.routes[0].location[2];
+                            tmp['state'] = route.routes[0].location[0];
+                            bigTmp2.splice(i, 1, tmp);
+                        })
+                }
+            })
+            .then(toDos => {
+                this.toDoList = bigTmp2;
             });
     },
 
