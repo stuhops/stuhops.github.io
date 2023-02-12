258b82ca63fad8b3886962de5001081c524be96e
Stuart Hopkins <stuarth@gurutechnologies.net>
2023-02-11 18:38:18 -0700

    Added linter and formatted

---
+++ src/app/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from '../app-routing/app-routing.module';
+import { MaterialModule } from '../material/material.module';
+import { AppComponent } from './components/app/app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, MaterialModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
