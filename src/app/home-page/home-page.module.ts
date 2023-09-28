d965713b897fa114e2063a2528f9f6710cce0305
Stuart Hopkins <stuarth@gurutechnologies.net>
2023-09-28 14:58:26 -0600

    Added a single image statement component

--- src/app/home-page/home-page.module.ts
+++ src/app/home-page/home-page.module.ts
@@ -3,9 +3,10 @@ import { HomePageComponent } from './components/home-page/home-page.component';
 import { BannerImageComponent } from './components/banner-image/banner-image.component';
 import { HomePageRouterModule } from './home-page.router';
 import { SharedModule } from '../shared/shared.module';
+import { SingleImageStatementComponent } from './components/single-image-statement/single-image-statement.component';
 
 @NgModule({
-  declarations: [HomePageComponent, BannerImageComponent],
+  declarations: [HomePageComponent, BannerImageComponent, SingleImageStatementComponent],
   imports: [HomePageRouterModule, SharedModule],
 })
 export class HomePageModule {}
