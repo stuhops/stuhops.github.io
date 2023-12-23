4f81e13df7e58487125e7b8ac6325eb1a208d1cc
Stuart Hopkins <stuarth@gurutechnologies.net>
2023-12-23 10:16:48 -0700

    Added a console log with the version number

--- src/environments/environment.interface.ts
+++ src/environments/environment.interface.ts
@@ -1,4 +1,6 @@
 export interface EnvironmentInterface {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  version: string;
   assetPrefix: 'assets/local/' | 'https://stu-resume-website.s3.us-west-1.amazonaws.com/';
   resume: {
     download: string;
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,8 @@
 import { EnvironmentInterface } from './environment.interface';
 
 export const environment: EnvironmentInterface = {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  version: `${require('../../package.json').version}`,
   assetPrefix: 'https://stu-resume-website.s3.us-west-1.amazonaws.com/',
   resume: {
     download: 'resume.pdf',
