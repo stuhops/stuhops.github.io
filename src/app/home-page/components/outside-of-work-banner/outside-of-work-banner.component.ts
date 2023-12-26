18b66eb5df0a8d488c48e41990faa0dc58358ef0
Stuart Hopkins <stuarth@gurutechnologies.net>
2023-12-25 19:51:06 -0700

    Updated the outside of work pictures and styling

--- src/app/home-page/components/outside-of-work-banner/outside-of-work-banner.component.ts
+++ src/app/home-page/components/outside-of-work-banner/outside-of-work-banner.component.ts
@@ -9,15 +9,36 @@ import { environment } from 'src/environments/environment';
 export class OutsideOfWorkBannerComponent {
   photos: { image: string; subtitle: string }[] = [
     {
-      image: `${environment.assetPrefix}code-sample.png`,
-      subtitle: 'Hello World. This is a long caption',
+      image: `${environment.assetPrefix}outside-of-work/climbing-small.jpg`,
+      subtitle: 'My favorite sport is climbing',
     },
-    { image: `${environment.assetPrefix}code-sample.png`, subtitle: 'Hello World' },
-    { image: `${environment.assetPrefix}code-sample.png`, subtitle: 'Hello World' },
-    { image: `${environment.assetPrefix}code-sample.png`, subtitle: 'Hello World' },
-    { image: `${environment.assetPrefix}code-sample.png`, subtitle: 'Hello World' },
-    { image: `${environment.assetPrefix}code-sample.png`, subtitle: 'Hello World' },
-    { image: `${environment.assetPrefix}code-sample.png`, subtitle: 'Hello World' },
-    { image: `${environment.assetPrefix}code-sample.png`, subtitle: 'Hello World' },
+    {
+      image: `${environment.assetPrefix}outside-of-work/kayaking-small.jpg`,
+      subtitle: 'I love to kayak',
+    },
+    {
+      image: `${environment.assetPrefix}outside-of-work/zion-small.jpg`,
+      subtitle: 'Zion national park is my second home',
+    },
+    {
+      image: `${environment.assetPrefix}outside-of-work/el-cap-small.jpg`,
+      subtitle: 'In 2023 I climbed El Capitan in Yosemite!',
+    },
+    {
+      image: `${environment.assetPrefix}outside-of-work/guiding-small.jpg`,
+      subtitle: 'I also love to guide in, teach about, and share with others how to get outside',
+    },
+    {
+      image: `${environment.assetPrefix}outside-of-work/rowing-small.jpg`,
+      subtitle: 'I enjoy long multi-day river trips',
+    },
+    {
+      image: `${environment.assetPrefix}outside-of-work/biology-small.jpg`,
+      subtitle: 'I love biology and foraging for wild things to eat',
+    },
+    {
+      image: `${environment.assetPrefix}outside-of-work/backpacking-small.jpg`,
+      subtitle: 'I like to backpack and reach beautiful places',
+    },
   ];
 }
