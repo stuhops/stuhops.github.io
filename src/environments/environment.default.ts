4367d962f22e2c50e5ed4a969e1a6672529badf7
Stuart Hopkins <stwhop@gmail.com>
2024-01-21 16:36:20 -0700

    Updated to have a default environment that you override individual things as needed

---
+++ src/environments/environment.default.ts
@@ -0,0 +1,19 @@
+import { EnvironmentInterface } from './environment.interface';
+
+export const defaultEnvironment: EnvironmentInterface = {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  version: `${require('../../package.json').version}`,
+  assetPrefix: 'https://stu-resume-website.s3.us-west-1.amazonaws.com/',
+  breakpoints: {
+    xs: 360,
+    small: 440,
+    medium: 760,
+    large: 1024,
+    xl: 1440,
+  },
+  resume: {
+    download: 'resume.pdf',
+    email: 'stuart.hops@gmail.com',
+    github: 'https://github.com/stuhops',
+  },
+};
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,19 +1,6 @@
+import { defaultEnvironment } from './environment.default';
 import { EnvironmentInterface } from './environment.interface';
 
-export const environment: EnvironmentInterface = {
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  version: `${require('../../package.json').version}`,
-  assetPrefix: 'https://stu-resume-website.s3.us-west-1.amazonaws.com/',
-  breakpoints: {
-    xs: 360,
-    small: 440,
-    medium: 760,
-    large: 1024,
-    xl: 1440,
-  },
-  resume: {
-    download: 'resume.pdf',
-    email: 'stuart.hops@gmail.com',
-    github: 'https://github.com/stuhops',
-  },
-};
+const overrides: Partial<EnvironmentInterface> = {};
+
+export const environment: EnvironmentInterface = Object.assign(defaultEnvironment, overrides);
